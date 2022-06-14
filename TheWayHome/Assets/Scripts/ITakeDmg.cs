29211ec3581b334ae3e7b1bb45342e8db91388d6
Macarena Ruiz <contactomruiz@yahoo.com>
2022-06-14 12:06:39 -0300

    script cambios

--- TheWayHome/Assets/Scripts/ITakeDmg.cs
+++ TheWayHome/Assets/Scripts/ITakeDmg.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ITakeDmg : MonoBehaviour
+public interface ITakeDmg
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void 
 }
