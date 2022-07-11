9f657245373f8e1e02d93cfb0d6d984b8e1530de
Macarena Ruiz <contactomruiz@yahoo.com>
2022-07-11 12:14:05 -0300

    arreglos contador y camara

--- TheWayHome/Assets/Scripts/puertaControl.cs
+++ TheWayHome/Assets/Scripts/puertaControl.cs
@@ -7,24 +7,18 @@ public class puertaControl : MonoBehaviour
 {
     [SerializeField] private int cantLlave = 10;
     private int llavesActuales;
-    private int item;
 
     [SerializeField] private Text ItemText;
 
     public void AbrirPuerta()
     {
         llavesActuales++;
+        ItemText.text = "Keys: " + llavesActuales;
 
-        if(llavesActuales >= cantLlave)
+        if (llavesActuales >= cantLlave)
         {
             Destroy(this.gameObject);
         }
-    }
-
-    void ContarLlavesText()
-    {
-        item++;
-        ItemText.text = "Keys: " + item;
     }
 
     private void OnEnable()
