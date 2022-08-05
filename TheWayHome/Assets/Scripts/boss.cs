81276286ab66fbe53a7d028551473c938bc7eb10
Macarena Ruiz <contactomruiz@yahoo.com>
2022-08-05 18:07:10 -0300

    ajustes + puerta boss

---
+++ TheWayHome/Assets/Scripts/boss.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss : MonoBehaviour
+{
+    public float maxSpeed = 4f;
+    public float speed = 4f;
+    private Rigidbody2D rb;
+    [SerializeField] int Health = 4;
+    public Transform target;
+    private bool mirandoDerecha = true;
+    [SerializeField] float agroRange;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float distToPlayer = Vector2.Distance(transform.position, target.position);
+
+        if (distToPlayer < agroRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            LookPlayer();
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+
+    public void LookPlayer()
+    {
+        if ((target.position.x > transform.position.x && !mirandoDerecha) || (target.position.x < transform.position.x && mirandoDerecha))
+        {
+            mirandoDerecha = !mirandoDerecha;
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+        }
+    }
+
+    public void Hit(int DamageTaken)
+    {
+        Health -= DamageTaken;
+
+        if (Health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Daño(int DamageTaken)
+    {
+        Hit(DamageTaken);
+    }
+}
