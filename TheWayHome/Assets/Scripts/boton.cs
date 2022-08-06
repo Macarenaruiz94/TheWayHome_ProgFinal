4f23d6e359a108bafa901c18d17ea05693749f68
Macarena Ruiz <contactomruiz@yahoo.com>
2022-08-06 15:35:54 -0300

    player limits

--- TheWayHome/Assets/Scripts/boton.cs
+++ TheWayHome/Assets/Scripts/boton.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class boton : MonoBehaviour
 {
     public string scene;
+
+    //load scene on click
     public void LoadScene()
     {
         SceneManager.LoadScene(scene);
     }
 
+    //exit game on click
     public void QuitGame()
     {
         Application.Quit();
--- TheWayHome/Assets/Scripts/cameraScript.cs
+++ TheWayHome/Assets/Scripts/cameraScript.cs
@@ -7,19 +7,22 @@ public class cameraScript : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] float timeOffset;
     [SerializeField] Vector2 posOffset;
-    void Start()
-    {
-        
-    }
+    [SerializeField] float leftLimit;
+    [SerializeField] float rightLimit;
+    [SerializeField] float bottomLimit;
+    [SerializeField] float topLimit;
 
-    // Update is called once per frame
     void Update()
     {
+        //movement follow player
         Vector3 startPos = transform.position;
         Vector3 endPos = player.transform.position;
         endPos.x += posOffset.x;
         endPos.y += posOffset.y;
         endPos.z = -10;
         transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        //limits on screen scene
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftLimit, rightLimit), Mathf.Clamp(transform.position.y, bottomLimit, topLimit), transform.position.z);
     }
 }
